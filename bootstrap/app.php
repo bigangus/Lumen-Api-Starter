859d2bacf5e63d0b7b1709789475d6e7f0941d11
Hongfei ZHANG <zhf@Mac-mini.local>
2024-01-22 17:43:22 +0800

    init

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -97,6 +97,7 @@ $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(App\Providers\EventServiceProvider::class);
 $app->register(Illuminate\Redis\RedisServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
+$app->register(LaravelLang\Publisher\ServiceProvider::class);
 
 if ($app->environment() == 'local') {
     $app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
