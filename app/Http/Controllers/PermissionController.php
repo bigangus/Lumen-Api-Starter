e4757d6cb3b1f64d966901c32bd4be894ed67b32
Hongfei ZHANG <zhf@Mac-mini.local>
2024-01-24 11:03:31 +0800

    init

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Http\Responses\Facade\HttpResponse;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class PermissionController extends Controller
 {
     public function list() : Response
     {
-        // TODO: Implement list() method.
-        return HttpResponse::success('Permission List');
+        $permissions = Auth::user()->getAllPermissions()->map(function($item) {
+            return [
+                'name' => $item['name'],
+                'translation' => __($item['name'])
+            ];
+        })->whereNotIn('name', config('permission.basic'))->values();
+
+        return HttpResponse::success('Get permissions success', [
+            'permissions' => $permissions->toArray()
+        ]);
     }
 }
