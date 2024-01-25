d9e8853d0c3d6ad10df46f6935c5feb214513fcc
Hongfei ZHANG <zhf@Mac-mini.local>
2024-01-25 11:06:33 +0800

    init

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,6 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->command('inspire')->hourly();
+         $schedule->command(GeneratePermissions::class)->hourly();
     }
 }
