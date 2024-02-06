1c4f7b734ab549ac1617604668424aa31dc2ad90
Hongfei ZHANG <zhf@Mac-mini.local>
2024-02-06 09:26:45 +0800

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,11 +1,14 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use App\Http\Responses\Facade\HttpResponse;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
@@ -43,10 +46,36 @@ class UserController extends Controller
         return HttpResponse::success('User Disabled');
     }
 
+    /**
+     * @throws ValidationException
+     */
     public function update(Request $request): Response
     {
-        // TODO: Implement update() method.
-        return HttpResponse::success('User Updated');
+        $this->validate($request, [
+            'id' => 'required|integer',
+            'phone' => 'nullable|string',
+            'email' => 'nullable|email'
+        ]);
+
+        $user = User::query()->find($request->input('id'));
+
+        if (!$user) {
+            return HttpResponse::error('User not found');
+        }
+
+        if ($request->has('phone')) {
+            $user->phone = $request->input('phone');
+        }
+
+        if ($request->has('email')) {
+            $user->email = $request->input('email');
+        }
+
+        $user->save();
+
+        return HttpResponse::success('User Updated', [
+            'user' => $user
+        ]);
     }
 
     public function assignRole(Request $request): Response
